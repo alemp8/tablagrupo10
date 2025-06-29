66a1cd205761d1aa980b35d30cfadf79b0355ce2
Victor Alejandro <alejandromeza008@gmail.com>
2025-06-28 23:39:47 -0600

    Cambios para deploy

--- tablagrupo10/vite.config.js
+++ tablagrupo10/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
+  base: '/tablagrupo10/', 
   plugins: [
     handlebars({
       partialDirectory: './src/partials'
